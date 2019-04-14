7e96006ae0e4480c9c78842c2c074f862ffd73a5
Marija Prokic <marijapanovic@yahoo.com>
2019-04-14 23:18:39 +0200

    Create posts in progress

--- bit-book/src/app/components/Header.js
+++ bit-book/src/app/components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {Dropdown, Button} from 'react-materialize';
 
 const Header = () => {
     return (
         <>
+       
             <nav>
                 <div className="nav-wrapper">
                     <Link to='/'>
                         <a className="brand-logo">Bitbook</a>
                     </Link>
                     <ul id="nav-mobile" className="right">
-
-                        <li><Link to='/'>Posts</Link></li>
+                        <li>
+                        <Dropdown className="nav-wrapper" trigger={<Link className="dropdownbtn">Posts</Link>}><a href="#">Post</a><a href="#">Image</a><a href="#">Video</a></Dropdown>
+                        <i className="material-icons right"></i></li>
                         <li><Link to='/people'>People</Link></li>
                         <li><Link to='/profile'>Profile</Link></li>
 
--- bit-book/src/app/posts/PostPage.js
+++ bit-book/src/app/posts/PostPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Modal } from 'react-materialize';
 import { fetchPosts } from '../../services/fetchPosts';
-import PostList from './PostList'
+import PostList from './PostList';
+import {Link} from 'react-router-dom';
+import { ModalAdd } from './ModalAdd';
 
 class PostPage extends React.Component {
     constructor(props) {
@@ -20,8 +23,18 @@ class PostPage extends React.Component {
 
     render() {
 
+        const addButton = <Link to={ModalAdd} className="btn-floating btn-large halfway-fab waves-effect">
+            <i className="material-icons">add</i>
+        </Link> 
+
         return (
-            < PostList posts={this.state.posts} />
+            <>
+            <PostList posts={this.state.posts} />
+            {/* <ModalAdd /> */}
+            <Modal header="New Post" trigger={addButton}>
+               <input  placeholder="insert text"></input>
+            </Modal>
+            </>
         )
     }
 }
