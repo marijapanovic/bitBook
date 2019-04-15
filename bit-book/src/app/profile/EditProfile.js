8e26ef49d0198a60b053d5531c2648086420399c
BIT Student <classroom@bgit.rs>
2019-04-15 10:27:24 +0200

    edit page work

--- bit-book/src/app/profile/EditProfile.js
+++ bit-book/src/app/profile/EditProfile.js
@@ -10,6 +10,7 @@ import Col from "react-bootstrap/Col";
 // import updateProfile from '../../services/updateProfile';
 
 import "./EditProfile.css";
+import updateProfile from "../../services/updateProfile";
 
 class EditProfile extends React.Component {
   constructor(props) {
@@ -19,257 +20,259 @@ class EditProfile extends React.Component {
     // this.handleClose = this.handleClose.bind(this);
 
     this.state = {
-        show: props.show,
-        user: props.user,
-        handleClose: props.handleClose,
-        handleShow: props.handleShow,
-        getUser: props.getUser
+      show: props.show,
+      user: props.user,
+      handleClose: props.handleClose,
+      handleShow: props.handleShow,
+      getUser: props.getUser
 
-        //   avatarUrl: "",
-        //   namePrefix: "",
-        //   nameFirst: "",
-        //   nameLast: "",
-        //   aboutBio: "",
-        //   aboutJob: "",
-        //   aboutCountryCode: ""
+      //   avatarUrl: "",
+      //   namePrefix: "",
+      //   nameFirst: "",
+      //   nameLast: "",
+      //   aboutBio: "",
+      //   aboutJob: "",
+      //   aboutCountryCode: ""
+    };
+  }
 
-        }
+  handleClose() {
+    this.setState({
+      show: false
+    });
+  }
+
+  handleShow() {
+    this.setState({
+      show: true
+    });
+  }
+
+  editProfileInfo = event => {
+    event.preventDefault();
+    const { user } = this.state;
+    // const userProfile = {
+    //   ...user,
+    //   namePrefix: this.user.namePrefix,
+    //   nameFirst: this.user.nameFirst,
+    //   nameLast: this.user.nameLast,
+    //   aboutBio: this.user.aboutBio,
+    //   aboutJob: this.user.aboutJob,
+    //   aboutCountryCode: this.user.aboutCountryCode
+    // };
+
+    const updatedUser = {
+      name: {
+        first: user.nameFirst,
+        last: user.nameLast
       }
-    
+    };
 
-    handleClose() {
-      this.setState({
-        show: false,
-      });
-    }
-  
-    handleShow() {
-      this.setState({ 
-        show: true
-      });
-    }
+    updateProfile(user.id, updatedUser)
+      .then(res => {
+        window.location.reload();
+      })
+      .catch(err => {});
+  };
 
-    editProfileInfo = (event) => {
-      const { user } = this.state;
-      const userProfile = {
-              ...user,
-              namePrefix: this.user.namePrefix,
-              nameFirst: this.user.nameFirst,
-              nameLast: this.user.nameLast,
-              aboutBio: this.user.aboutBio,
-              aboutJob: this.user.aboutJob,
-              aboutCountryCode: this.user.aboutCountryCode,
-          }
-      this.setState({
-        user: userProfile
-          });
-    }
-      
-      // this.setState(prevState => ({
-      //     user: {
-      //         ...prevState.user,
-      //         namePrefix: this.user.namePrefix,
-      //         nameFirst: this.user.nameFirst,
-      //         nameLast: this.user.nameLast,
-      //         aboutBio: this.user.aboutBio,
-      //         aboutJob: this.user.aboutJob,
-      //         aboutCountryCode: this.user.aboutCountryCode,
-      //     }
-      // }))}
-  
+  // this.setState(prevState => ({
+  //     user: {
+  //         ...prevState.user,
+  //         namePrefix: this.user.namePrefix,
+  //         nameFirst: this.user.nameFirst,
+  //         nameLast: this.user.nameLast,
+  //         aboutBio: this.user.aboutBio,
+  //         aboutJob: this.user.aboutJob,
+  //         aboutCountryCode: this.user.aboutCountryCode,
+  //     }
+  // }))}
 
-    changePrefixName = (event) => {
-      const { user } = this.state;
-      const userPrefix = {
-          ...user,
-          namePrefix: event.target.value,
-      }
+  changePrefixName = event => {
+    const { user } = this.state;
+    const userPrefix = {
+      ...user,
+      namePrefix: event.target.value
+    };
     this.setState({
       user: userPrefix
     });
   };
-  
-  changeFirstName = (event) => {
+
+  changeFirstName = event => {
     const { user } = this.state;
     const userFirstName = {
-        ...user,
-        nameFirst: event.target.value,
-    }
-  this.setState({
-    user: userFirstName
-  });
+      ...user,
+      nameFirst: event.target.value
+    };
+    this.setState({
+      user: userFirstName
+    });
   };
-  
-  changeLastName = (event) => {
+
+  changeLastName = event => {
     const { user } = this.state;
     const userLastName = {
-        ...user,
-        nameLast: event.target.value,
-    }
-  this.setState({
-    user: userLastName
-  });
+      ...user,
+      nameLast: event.target.value
+    };
+    this.setState({
+      user: userLastName
+    });
   };
-  
-  changeAboutBio = (event) => {
+
+  changeAboutBio = event => {
     const { user } = this.state;
     const userAboutBio = {
-        ...user,
-        aboutBio: event.target.value,
-    }
-  this.setState({
-    user: userAboutBio
-  });
+      ...user,
+      aboutBio: event.target.value
+    };
+    this.setState({
+      user: userAboutBio
+    });
   };
-  
-  changeAboutJob = (event) => {
+
+  changeAboutJob = event => {
     const { user } = this.state;
     const userAboutJob = {
-        ...user,
-        aboutJob: event.target.value,
-    }
-  this.setState({
-    user: userAboutJob
-  });
+      ...user,
+      aboutJob: event.target.value
+    };
+    this.setState({
+      user: userAboutJob
+    });
   };
-  
-  changeAboutCountryCode = (event) => {
+
+  changeAboutCountryCode = event => {
     const { user } = this.state;
     const userAboutCountryCode = {
-        ...user,
-        aboutCountryCode: event.target.value,
-    }
-  this.setState({
-    user: userAboutCountryCode
-  });
+      ...user,
+      aboutCountryCode: event.target.value
+    };
+    this.setState({
+      user: userAboutCountryCode
+    });
   };
 
-
   render() {
-    
     const { user, show } = this.props;
     const {
-        avatarUrl,
-        namePrefix,
-        nameFirst,
-        nameLast,
-        aboutBio,
-        aboutJob,
-        aboutCountryCode
+      avatarUrl,
+      namePrefix,
+      nameFirst,
+      nameLast,
+      aboutBio,
+      aboutJob,
+      aboutCountryCode
     } = this.state.user;
 
-    if(!show) {
-        return null;
+    if (!show) {
+      return null;
     }
 
     return (
       <>
-              <Modal show={show} onHide={this.handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>UPDATE PROFILE</Modal.Title>
-                </Modal.Header>
+        <Modal show={show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>UPDATE PROFILE</Modal.Title>
+          </Modal.Header>
 
-                <Modal.Body>
-                  <Form>
-                    <Form.Group controlId="formGroupPicture">
-                      <Row>
-                        <Col xs={6} md={4}>
-                          <Image
-                            src={avatarUrl}
-                            roundedCircle
-                          />
-                        </Col>
-                      </Row>
-                      <Button variant="primary">Upload Image</Button>
-                    </Form.Group>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="formGroupPicture">
+                <Row>
+                  <Col xs={6} md={4}>
+                    <Image src={avatarUrl} roundedCircle />
+                  </Col>
+                </Row>
+                <Button variant="primary">Upload Image</Button>
+              </Form.Group>
 
-                    <Form.Group controlId="formGroupName">
-                      <Row />
+              <Form.Group controlId="formGroupName">
+                <Row />
 
-                      <Row>
-                        <Col>
-                          <Form.Label>Prefix</Form.Label>
-                          <Form.Control
-                            type="text"
-                            placeholder="Prefix"
-                            value={namePrefix}
-                            name={namePrefix}
-                            onChange={this.changePrefixName}
-                            
-                          />
-                        </Col>
-                        <Col>
-                          <Form.Label>First Name</Form.Label>
-                          <Form.Control
-                            type="text"
-                            placeholder="First Name"
-                            value={nameFirst}
-                            name={nameFirst}
-                            onChange={this.changeFirstName}
-                          />
-                        </Col>
-                        <Col>
-                          <Form.Label>Last Name</Form.Label>
-                          <Form.Control
-                            type="text"
-                            placeholder="Last Name"
-                            value={nameLast}
-                            name={nameLast}
-                            onChange={this.changeLastName}
-                          />
-                        </Col>
-                      </Row>
-                    </Form.Group>
+                <Row>
+                  <Col>
+                    <Form.Label>Prefix</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Prefix"
+                      value={namePrefix}
+                      name={namePrefix}
+                      onChange={this.changePrefixName}
+                    />
+                  </Col>
+                  <Col>
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="First Name"
+                      value={nameFirst}
+                      name={nameFirst}
+                      onChange={this.changeFirstName}
+                    />
+                  </Col>
+                  <Col>
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Last Name"
+                      value={nameLast}
+                      name={nameLast}
+                      onChange={this.changeLastName}
+                    />
+                  </Col>
+                </Row>
+              </Form.Group>
 
-                    <Form.Group controlId="formGroupName">
-                      <Row>
-                        <Form.Label>Description</Form.Label>
-                      </Row>
-                      <Row>
-                        <Col>
-                          <Form.Label>Bio</Form.Label>
-                          <Form.Control
-                            placeholder="Bio"
-                            value={aboutBio}
-                            name={aboutBio}
-                            onChange={this.changeAboutBio}
-                          />
-                        </Col>
-                        <Col>
-                          <Form.Label>Job</Form.Label>
-                          <Form.Control
-                            placeholder="Job"
-                            value={aboutJob}
-                            name={aboutJob}
-                            onChange={this.changeAboutJob}
-                          />
-                        </Col>
-                        <Col>
-                          <Form.Label>Country Code</Form.Label>
-                          <Form.Control
-                            placeholder="Country Code"
-                            value={aboutCountryCode}
-                            name={aboutCountryCode}
-                            onChange={this.changeAboutCountryCode}
-                          />
-                        </Col>
-                      </Row>
-                    </Form.Group>
-                  </Form>
-                </Modal.Body>
+              <Form.Group controlId="formGroupName">
+                <Row>
+                  <Form.Label>Description</Form.Label>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Label>Bio</Form.Label>
+                    <Form.Control
+                      placeholder="Bio"
+                      value={aboutBio}
+                      name={aboutBio}
+                      onChange={this.changeAboutBio}
+                    />
+                  </Col>
+                  <Col>
+                    <Form.Label>Job</Form.Label>
+                    <Form.Control
+                      placeholder="Job"
+                      value={aboutJob}
+                      name={aboutJob}
+                      onChange={this.changeAboutJob}
+                    />
+                  </Col>
+                  <Col>
+                    <Form.Label>Country Code</Form.Label>
+                    <Form.Control
+                      placeholder="Country Code"
+                      value={aboutCountryCode}
+                      name={aboutCountryCode}
+                      onChange={this.changeAboutCountryCode}
+                    />
+                  </Col>
+                </Row>
+              </Form.Group>
+            </Form>
+          </Modal.Body>
 
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={this.props.handleClose}>
-                    Close
-                  </Button>
-                  <Button variant="primary" onClick={this.props.editProfileInfo}>
-                    Update
-                  </Button>
-                </Modal.Footer>
-              </Modal>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.props.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.editProfileInfo}>
+              Update
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </>
     );
-    }
+  }
 }
 
 export default EditProfile;
--- bit-book/src/services/updateProfile.js
+++ bit-book/src/services/updateProfile.js
@@ -1,16 +1,16 @@
-import { API_BASE_URL } from '../shared/constants';
+import { API_BASE_URL } from "../shared/constants";
 
-const updateProfile = (id) => {
-    fetch(`${API_BASE_URL}/users/${id}`, {
-        method: 'PATCH',
-        headers: {
-            "content-type": "application/json",
-            "x-api-key": "B1tD3V"
-        },
-        body: JSON.stringify(id)
-    })
-        .then(res => res.json())
-        .then(data => data)
-}
+const updateProfile = (id, body) => {
+  return fetch(`${API_BASE_URL}/users/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": "B1tD3V"
+    },
+    body: JSON.stringify(body)
+  })
+    .then(res => res.json())
+    .then(data => data);
+};
 
 export default updateProfile;
