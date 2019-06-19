6b7a52f4373c63ac59d44cbf07146e6c7eb89786
mprokic <marijapanovic1@gmail.com>
2019-06-19 22:11:38 +0200

    CORS fix

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,6 +1,6 @@
  //export const API_BASE_URL = "https://book-api.hypetech.xyz/v1";
 
-export const API_BASE_URL = "//book-api-dev.hypetech.xyz";
+export const API_BASE_URL = "https://cors.io/?http://book-api-dev.hypetech.xyz";
 
 export const HEADERS_API = {
     headers: {
